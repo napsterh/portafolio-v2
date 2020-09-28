0461bb4789620976f1f0fbe3dc3f697b9c93d5c9
Napster <rome.1205h@gmail.com>
2020-09-28 03:04:36 -0500

    update de navbar

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/pages/Navbar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Particles from 'react-particles-js';
 
 function App() {
   return (
-    <Router>
-      <Navbar/>
-      <Switch>
-        <Router path="/"/>
-      </Switch>
-    </Router>
+      <Router>
+        <Navbar/>
+        <Switch>
+          <Router path="/"/>
+        </Switch>
+      </Router>
   );
 }
 
